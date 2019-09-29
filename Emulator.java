c26fcd3fe053310177ef5b3b568d1d51761bd97b
kevinfeng1999@gmail.com <32528760+kevinfwl@users.noreply.github.com>
2019-09-29 19:46:40 -0400

    added some opcodes

--- Emulator.java
+++ Emulator.java
@@ -9,6 +9,7 @@ public class Emulator{
     private short opcode;
 
     private byte[] memory;
+    private int[] V;
     private short pc;
     private short I;
 
@@ -21,10 +22,13 @@ public class Emulator{
     private short[] stack;
     private byte sp;
 
+    private boolean drawFlag;
+
     private Emulator() {
         this.memory = new byte[4096];
         this.fb = new boolean[64 *32];
         this.stack = new short[16];
+        this.V = new int[16];
     }
 
     public static Emulator getInstance() {
@@ -49,21 +53,27 @@ public class Emulator{
         this.I = 0;
         this.sp = 0;
         this.pc = 0x200;
+        this.drawFlag = false;
     }
 
     public void fetch() {
         this.opcode = (short) (this.memory[this.pc] << 8 | this.memory[this.pc + 1]);
-        this.sp += 2;
-        System.out.print("0x");
-        System.out.print(intToString(opcode >> 12));
-        System.out.print(intToString((opcode >> 8) & 0xF));
-        System.out.print(intToString((opcode >> 4) & 0xF));
-        System.out.print(intToString(opcode & 0xF));
-        System.out.println("");
     }
 
     public void execute() {
-        
+        switch ((this.opcode >> 12) & 0xf) {
+            case 0x0:
+
+                break;
+
+
+            case 0x1:
+                break;
+
+
+            default:
+                break;
+        }
     }
 
     public void decrementTimer() {
@@ -117,6 +127,103 @@ public class Emulator{
     }
 
     private void beep() {
+        
+    }
+
+
+
+    //private opcode functions
+    private void zero() {
+        switch (this.opcode) {
+            case 0x00E0:
+                for ( boolean pixel : this.fb) {
+                    pixel = false;
+                }
+                this.drawFlag = true;
+                this.pc += 2;
+                return;
+            case 0x00EE:
+                this.pc = this.stack[this.sp];
+                this.sp--;
+                this.pc += 2;
+                this.drawFlag = true;
+                return;
+            default:
+                this.pc = (short) (this.opcode & 0x0fff);
+                return;
+        }
+    }
+
+    private void one() {
+        this.pc = (short) (this.opcode & 0x0fff);
+    }
+
+    private void two() {
+        this.stack[++this.sp] = pc;
+        this.pc = (short) (this.opcode & 0x0fff);
+        this.drawFlag = true;
+    }
+
+    private void three() {
+        if (this.V[(this.opcode >>> 8) & 0xf] == (this.opcode & 0x00ff)) pc += 2;
+        pc += 2;
+    }
+
+    private void four() {
+        if (this.V[(this.opcode >>> 8) & 0xf] != (this.opcode & 0x00ff)) pc += 2;
+        pc += 2;
+    }
+
+    private void five() {
+        if (this.V[(this.opcode >>> 8) & 0xf] == this.V[(this.opcode >>> 4) & 0xf]) pc += 2;
+        pc += 2;
+    }
+
+    private void six() {
+        this.V[(this.opcode >>> 8) & 0xf] = (byte) (this.opcode & 0x00ff);
+        this.pc +=2;
+    }
+
+    private void seven() {
+        this.V[(this.opcode >>> 8) & 0xf] += (byte) (this.opcode & 0x00ff);
+        this.pc +=2;
+    }
+
+    private void eight() {
+        byte Vx = this.V[(this.opcode >>> 8) & 0xf];
+        byte Vy = this.V[(this.opcode >>> 4) & 0xf];
+        switch (this.opcode & 0xf) {
+            case 0x0:
+                this.V[(this.opcode >>> 8) & 0xf] = this.V[(this.opcode >>> 4) & 0xf];
+                break;
+            case 0x1:
+            this.V[(this.opcode >>> 8) & 0xf] this.V[(this.opcode >>> 8) & 0xf] | this.V[(this.opcode >>> 4) & 0xf];
+            case 0x2:
+
+                break;
+            case 0x3:
+                break;
+            case 0x4:
+                break;
+            case 0x5:
+                break;
+            case 0x6:
+                break;
+            case 0x7:
+                break;
+            case 0xE:
+                break;
+            default:
+        }
+        this.pc += 2;
+    }
+
+    private void nine() {
+        if (this.V[(this.opcode >>> 12) & 0xf] != this.V[(this.opcode >>> 8) & 0xf]) this.pc += 2;
+        this.pc += 2;
+    }
+
+    private void A() {
         
     }
 }
